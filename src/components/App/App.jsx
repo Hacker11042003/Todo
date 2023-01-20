640a52dc1ce69b266be2161221010ebbe035a7dd
Programmer110403 <mejmanbekov72@gmail.com>
2023-01-20 13:16:59 +0600

    todolist1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,85 +6,108 @@ import TodoSearch from "../TodoSearch/TodoSearch";
 
 class App extends Component {
   state = {
-    todos:[
-      {title:'Learn Js', completed:false, importand:false, id:1},
-      {title:'Learn React', completed:false, importand:false, id:2},
-      {title:'Learn Python', completed:false, importand:false, id:3},
-      {title:'Learn Node js', completed:false, importand:false, id:4}
+    todos: [
+      { title: "Learn Js", completed: false, importand: false, id: 1 },
+      { title: "Learn React", completed: false, importand: false, id: 2 },
+      { title: "Learn Python", completed: false, importand: false, id: 3 },
+      { title: "Learn Node js", completed: false, importand: false, id: 4 },
     ],
-    term: ''
-  }
+    term: "",
+    filter: "active", //all/active/done
+  };
 
-  addTodo = (newTodo)=>{
-    const oldId = this.state.todos.map(element=>element.id )
-      const todo  = {title:newTodo,completed:false, importand:false, id:oldId.at(-1)+1 || 1}
-      this.setState( {todos:[...this.state.todos,todo]})
-  }
+  addTodo = (newTodo) => {
+    const oldId = this.state.todos.map((element) => element.id);
+    const todo = {
+      title: newTodo,
+      completed: false,
+      importand: false,
+      id: oldId.at(-1) + 1 || 1,
+    };
+    this.setState({ todos: [...this.state.todos, todo] });
+  };
 
+  deleteTodo = (id) => {
+    this.setState({ todos: this.state.todos.filter((el) => el.id !== id) });
+  };
 
-  deleteTodo =(id)=>{
-    this.setState({todos:this.state.todos.filter(el=> el.id!==id)})
-
-  }
-
-  completeTodo = (id) =>{
-    const index = this.state.todos.findIndex(el=>el.id===id)
-    const oldTodo = this.state.todos[index]
-    const before = this.state.todos.slice(0, index)
-    const after = this.state.todos.slice(index +1)
+  completeTodo = (id) => {
+    const index = this.state.todos.findIndex((el) => el.id === id);
+    const oldTodo = this.state.todos[index];
+    const before = this.state.todos.slice(0, index);
+    const after = this.state.todos.slice(index + 1);
     const upTodo = {
-      ...oldTodo, completed: !oldTodo.completed
-    } 
-    this.setState({todos:[...before, upTodo, ...after]})
+      ...oldTodo,
+      completed: !oldTodo.completed,
+    };
+    this.setState({ todos: [...before, upTodo, ...after] });
     console.log(upTodo);
-  }
-  impotandTodo = (id) =>{
-    const index = this.state.todos.findIndex(el=>el.id===id)
-    const oldTodo = this.state.todos[index]
-    const before = this.state.todos.slice(0, index)
-    const after = this.state.todos.slice(index +1)
+  };
+  impotandTodo = (id) => {
+    const index = this.state.todos.findIndex((el) => el.id === id);
+    const oldTodo = this.state.todos[index];
+    const before = this.state.todos.slice(0, index);
+    const after = this.state.todos.slice(index + 1);
     const upTodo = {
-      ...oldTodo, importand: !oldTodo.importand
-    } 
-    this.setState({todos:[...before, upTodo, ...after]})
+      ...oldTodo,
+      importand: !oldTodo.importand,
+    };
+    this.setState({ todos: [...before, upTodo, ...after] });
     console.log(upTodo);
-  }
+  };
 
-  onSearchChange = ( term ) =>{
-    this.setState({term})
-
-  }
+  onSearchChange = (term) => {
+    this.setState({ term });
+  };
 
   search(items, term) {
     if (term.length === 0) {
-      return items
-      
+      return items;
     }
     return items.filter((item) => {
-      return item.title.toLowerCase().indexOf(term.toLowerCase())>-1
-    })
+      return item.title.toLowerCase().indexOf(term.toLowerCase()) > -1;
+    });
   }
+
+  filter(items, filter) {
+    switch (filter) {
+      case "all":
+        return items;
+      case "active":
+        return items.filter((item) => !item.completed);
+      case "done":
+        return items.filter((item) => item.completed);
+      default:
+        return items;
+    }
+  }
+  setStatus = (name) => {
+    this.setState({
+      filter: name,
+    });
+  };
+
   render() {
-
-    const {todos, term} = this.state
-    const visebleItems = this.search(todos, term)
-    const doneCount = todos.filter((el) =>el.completed).length //count
-    const allCount = todos.length-doneCount
-
+    const { todos, term, filter } = this.state;
+    const visebleItems = this.filter(this.search(todos, term), filter);
+    const doneCount = todos.filter((el) => el.completed).length; //count
+    const allCount = todos.length - doneCount;
 
     return (
       <div className="mx-auto" style={{ width: 700 }}>
-        <TodoHeader 
-        doneCount = {doneCount}
-        allCount = {allCount} />
-        <TodoSearch onSearchChange = {this.onSearchChange} />
-        <TodoList 
-        todoData = {visebleItems} 
-        onDelete = {this.deleteTodo}
-        onCompleted = {this.completeTodo}
-        onImportand = {this.impotandTodo}
+        <TodoHeader doneCount={doneCount} allCount={allCount} />
+        <TodoSearch
+          onSearchChange={this.onSearchChange}
+          filter={this.filter}
+          onsetStatus={this.setStatus}
         />
-        <TodoAdd onAddTodo = {this.addTodo}/>
+        <TodoList
+          todoData={visebleItems}
+          onDelete={this.deleteTodo}
+          onCompleted={this.completeTodo}
+          onImportand={this.impotandTodo}
+        />
+        <TodoAdd onAddTodo={this.addTodo} />
       </div>
     );
   }
--- src/components/TodoSearch/TodoSearch.jsx
+++ src/components/TodoSearch/TodoSearch.jsx
@@ -1,35 +1,48 @@
-import React, { Component } from 'react'
-import './TodoSearch.css'
+import React, { Component } from "react";
+import "./TodoSearch.css";
 class TodoSearch extends Component {
-  state={
-    term: ''
-  }
-  onSearchChange = (e) =>{
-    const term  = e.target.value
-    this.setState({term})
-    this.props.onSearchChange(term)
+  buttons = [
+    { name: "all", title: "All" },
+    { name: "active", title: "Active" },
+    { name: "done", title: "Done" },
+  ];
+  state = {
+    term: "",
+  };
+  onSearchChange = (e) => {
+    const term = e.target.value;
+    this.setState({ term });
+    this.props.onSearchChange(term);
+  };
 
-
-  }
-
-  render(){
+  render() {
+    const { filter } = this.props;
+    const buttons = this.buttons.map(({ name, title }) => {
+      const isActive = filter === name;
+      const clazz = isActive ? "btn-info" : "btn-outline-secondary";
+      return (
+        <button
+          onClick={() => this.props.onsetStatus(name)}
+          className={`btn ${clazz} `}
+          key={name}
+        >
+          {title}
+        </button>
+      );
+    });
     return (
-      <div className='search'>
-        <input 
-        type="text" 
-        placeholder='Enter todo'
-        className='input-input'
-        value={this.state.term}
-        onChange = {this.onSearchChange}
-         />
-         <div>
-         <button>All</button>
-         <button>Active</button>
-         <button>Done</button>
-         </div>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Enter todo"
+          className="input-input"
+          value={this.state.term}
+          onChange={this.onSearchChange}
+        />
+        <div>{buttons}</div>
       </div>
-    )
+    );
   }
 }
 
-export default TodoSearch
+export default TodoSearch;
